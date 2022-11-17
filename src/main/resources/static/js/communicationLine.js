eb09238cff9848b5742618d42cc38ceeeef93dd8
kirki <kirki@LAPTOP-7JIM99T4>
2022-11-17 14:38:28 -0500

    updated css and js code content

---
+++ src/main/resources/static/js/communicationLine.js
@@ -0,0 +1,38 @@
+var textBox = document.querySelector("#messageBox")
+setInterval(retrieveMessages, 500)
+textBox.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        let message = {
+                "text": textBox.value,
+                "channelId": channelId,
+                "user": user,
+                "createdDate": new Date()
+        }
+        let messageText = textBox.value
+        console.log(`Send message ${messageText}`)
+        textBox.value = ''
+        fetch('/messages', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            body: JSON.stringify(message)
+        }).then(response => {retrieveMessages()})
+        return false
+    }
+})
+
+function retrieveMessages () {
+    let messageContainer = document.querySelector(".communication-container")
+    fetch(`/messages/${channelId}`)
+    .then(response => response.json())
+    .then(messages => {
+        messageContainer.innerHTML = ''
+        messages.forEach(message => {
+            messageContainer.innerHTML += `<div>
+              <span class="timestamp">${message.user.name}: </span>
+              <span class="message">${message.text}</span>
+            </div>`
+        })
+    })
+}
