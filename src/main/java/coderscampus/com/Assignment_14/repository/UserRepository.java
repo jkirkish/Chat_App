d2da1bd004db208a712534008120898ae6fceba3
kirki <kirki@LAPTOP-7JIM99T4>
2022-11-17 15:43:48 -0500

    User Repository update

--- src/main/java/coderscampus/com/Assignment_14/repository/UserRepository.java
+++ src/main/java/coderscampus/com/Assignment_14/repository/UserRepository.java
@@ -1,14 +1,13 @@
-package coderscampus.com.Assignment_14.repositorimport
+package coderscampus.com.Assignment_14.repository;
 
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
 import org.springframework.stereotype.Component;
-
 import com.coderscampus.Assignment14.dto.User;
-
-org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component;
 
 	
 
@@ -17,14 +16,15 @@ org.springframework.stereotype.Component;
 @Component
 public class UserRepository {
 
-	private Set<User> users = new LinkedHashSet<>();
+	
+	private ArrayList<User>users = new ArrayList<>();
 	
 	public User save (User user) {
 		if (users.size() == 0) {
 			user.setId(1L);
 		} else {
-			User EndUser = ((TreeSet<User>)users).last();
-			user.setId(EndUser.getId() + 1L);
+			User Endparticipant = users.get(users.size()-1);    
+			user.setId(Endparticipant.getId() + 1L);
 		}
 		users.add(user);
 		return user;
