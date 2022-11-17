59fa27a791bba4b4889d5581917fb60871c78da6
Joseph Kirkish <kirkish.2@gmail.com>
2022-11-16 23:59:50 -0500

    Assignment 14
    iniitial committ

---
+++ src/test/java/coderscampus/com/Assignment_14/Assignment14ApplicationTests.java
@@ -0,0 +1,13 @@
+package coderscampus.com.Assignment_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
