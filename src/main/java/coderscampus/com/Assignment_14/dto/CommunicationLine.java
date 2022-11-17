c44315706c048213c7e146a764648af0dfe4d563
kirki <kirki@LAPTOP-7JIM99T4>
2022-11-17 13:43:48 -0500

    body of dto code used

--- src/main/java/coderscampus/com/Assignment_14/dto/CommunicationLine.java
+++ src/main/java/coderscampus/com/Assignment_14/dto/CommunicationLine.java
@@ -1,5 +1,19 @@
 package coderscampus.com.Assignment_14.dto;
 
 public class CommunicationLine {
-
+	private Long channelId;
+	private String name;
+	
+	public Long getChannelId() {
+		return channelId;
+	}
+	public void setChannelId(Long channelId) {
+		this.channelId = channelId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String channelName) {
+		this.name = channelName;
+	}
 }
