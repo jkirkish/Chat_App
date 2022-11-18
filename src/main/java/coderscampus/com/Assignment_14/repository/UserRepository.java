89abe030f1375e3d6c82f11bb8b25ce4851ae5e9
kirki <kirki@LAPTOP-7JIM99T4>
2022-11-17 19:42:21 -0500

    UserRepository

--- src/main/java/coderscampus/com/Assignment_14/repository/UserRepository.java
+++ src/main/java/coderscampus/com/Assignment_14/repository/UserRepository.java
@@ -5,21 +5,17 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Component;
 
 import coderscampus.com.Assignment_14.dto.User;
-	
-
-
 
 @Component
 public class UserRepository {
 
-	
-	private ArrayList<User>users = new ArrayList<>();
-	
-	public User save( User user) {
+	private ArrayList<User> users = new ArrayList<>();
+
+	public User save(User user) {
 		if (users.size() == 0) {
 			user.setId(1L);
 		} else {
-			User Endparticipant = users.get(users.size()-1);    
+			User Endparticipant = users.get(users.size() - 1);
 			user.setId(Endparticipant.getId() + 1L);
 		}
 		users.add(user);
