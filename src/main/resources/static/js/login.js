67e80adc38896a56dab293ea9bfd077a23e14f7e
Joseph Kirkish <kirkish.2@gmail.com>
2022-11-19 22:01:11 -0500

    Update login.js
    added an array to push users

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,6 +2,7 @@ var submitBtn = document.querySelector("#submitBtn")
 console.log("submitBtn is: ")
 console.log(submitBtn)
 
+let users = []
 //function injected in the addEventlistener to make less lines of code
 //https://idratherbewriting.com/events-and-listeners-javascript/
 /*events and listeners
@@ -11,15 +12,26 @@ onmouseover //when a user mouses over something
 onfocus //when a user puts the cursor in a form field
 onblur //When a user leaves a form field
 */
-submitBtn.addEventListener('click', function(){
-	console.log("I have been clicked")
+submitBtn.addEventListener('click', ()=>{
+	let username = document.querySelector("#username")
+	let password = document.querySelector("#password")
+
+	if(username.value == '' || password.value == ''){
+		alert("You need to input a valid username and password")
+	}else{
+		console.log("Valid login. Enjoy!")
+		var user = {
+			"username" : username.value,
+			"password" : password.value
+		}
+		users.push(user);
+	}	
+})
+
 	
 /*
 th:field="${user.username}"/> will already have an id
 */
-var usernameTextBox = document.querySelector("#username")
-usernameTextBox.addEventListener('focus',function(){
-	console.log("The username textbox now has focus")
-})
-})
+
+
 
